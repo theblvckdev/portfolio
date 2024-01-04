11df94e07c82190c949c6f70e0a835889d238096
joeCodes05 <nathancodes05@gmail.com>
2024-01-04 19:36:34 +0100

    experience section completed

---
+++ src/data/experience_data.js
@@ -0,0 +1,22 @@
+export const experiences = [
+  {
+    title: "Frontend developer intern",
+    company: "GPRS Innovation",
+    duration: "Apr 2022 - Sep 2022",
+    content: `Helped deliver high-quality, productive code for a diverse array of projects
+    for clients. Also taught over 10 students the basics of frontend web
+    development, helping them to understand HTML, CSS & JavaScript.`,
+    tools: ["HTML", "CSS", "JavaScript", "Bootstrap"],
+  },
+
+  {
+    title: "Graphics Designer",
+    company: "Fix Impressions",
+    duration: "Feb 2023 - Nov 2023",
+    content: `Participated in designing unique magazines, invitation cards, flyers and
+    many more. Learned to use design tools like: corel draw, photoshop,
+    adobe illustrator. I was also given the privilege to teach 2 new employees
+    some of the tools used.`,
+    tools: ["Corel Draw", "Photoshop", "Adobe Illustrator"],
+  },
+];
--- src/pages/experience.jsx
+++ src/pages/experience.jsx
@@ -1,10 +1,57 @@
+import { experiences } from "../data/experience_data"
+import { MdArrowOutward } from "react-icons/md";
+import resume from '../assets/pdf/Resume.pdf'
+
 const ExperienceSection = () => {
   return (
     <>
-      <section className="text-white mt-20" id="experience">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro a possimus voluptatem. Eum temporibus at esse exercitationem dolorum? Sint esse corporis recusandae eaque beatae ducimus molestiae facere quam neque! Debitis.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro a possimus voluptatem. Eum temporibus at esse exercitationem dolorum? Sint esse corporis recusandae eaque beatae ducimus molestiae facere quam neque! Debitis.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro a possimus voluptatem. Eum temporibus at esse exercitationem dolorum? Sint esse corporis recusandae eaque beatae ducimus molestiae facere quam neque! Debitis.
+      <section data-aos="fade-up"
+        data-aos-duration="1000"
+        ata-aos-easing="ease-in" className="xl:mt-24 mt-20 text-gray-500" id="experience">
+        <h1 className="text-white font-semibold text-2xl lg:hidden block mb-5">
+          Experience
+        </h1>
+        <div className="flex flex-col space-y-5 gap-3">
+          {experiences.map((data, index) => {
+            const { title, company, content, duration, tools } = data;
+
+            return (
+              <div key={index} className="flex lg:flex-row flex-col gap-3 group ">
+                <div className="text-[12px] leading-6 basis-1/4 uppercase">
+                  {duration}
+                </div>
+                <div className="basis-3/4">
+                  <h1 className="text-[16px] group-hover:text-blue-300 duration-300 ease-in font-semibold text-white">
+                    {title} @ {company}
+                  </h1>
+
+                  <p className="mt-3 text-sm">
+                    {content}
+                  </p>
+
+                  <div className="mt-5 flex gap-2 items-center">
+                    {tools.map((data, index) => {
+                      return (
+                        <div key={index} className="py-1.5 px-3 bg-blue-500 bg-opacity-10 text-[12px] xl:text-[12px] lg:text-[8px] rounded-full text-blue-300">
+                          {data}
+                        </div>
+                      )
+                    })}
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+
+        <div className="mt-8">
+          <a href={resume} target="_blank" rel="noreferrer" className="no-underline w-fit group outline-none font-[500] text-base text-white duration-300 ease-in hover:text-blue-300 flex gap-2 items-center">
+            <div>
+              View Full Résumé
+            </div>
+            <MdArrowOutward className="duration-300 ease-in group-hover:-translate-y-1 group-hover:translate-x-1" />
+          </a>
+        </div>
       </section>
     </>
   )
